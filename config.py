0a95a850c5bbed6d81135cd0d391876a1d4e5f49
DaiSDiscord <72703334+DaiSDiscord@users.noreply.github.com>
2022-03-29 15:25:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    STANDARD_STATUS = "больше никогда не будет чувств"
+    STATUS = "Listening to Spotify | {track} {artist}  {album}"
+    VK_TOKEN = "5a74d7e5be6c436d8a046db2467372fae451a2479e606579bb6c557c16d674688263b9d469d5e184aab38"
+    CLIENT_ID = "f1f5bd124bea43209e1b9f07831854f1"
+    CLIENT_SECRET = "e3425685fb9946de88b1f99be5838b01"
+    REDIRECT_URI = "http://localhost:8888"
+    USERNAME = "depressed kid?"
+    SCOPE = "user-read-playback-state user-library-read"
